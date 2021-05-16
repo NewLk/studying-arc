5cc8ee05bfd8d61d768204fc6e73ed6ebc122eb9
Newton Ribeiro <newtonafr@hotmail.com>
2021-05-16 14:03:19 -0300

    Implementação do primeiro serviço
    O serviço de local storage utilizando shared preferences foi implemento em services, a interface foi definida em interface e o consumo do serviço foi feito no AppController. Como o shared preferences é um package, a sua instalação teve que ser feita em depencies no pubspec.yaml.

--- lib/app/services/shared_local_storage_service.dart
+++ lib/app/services/shared_local_storage_service.dart
@@ -1,1 +1,34 @@
-class SharedLocalStorageService {}
+import 'package:my_app/app/interfaces/local_storage_interface.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SharedLocalStorageService implements ILocalStorage {
+  @override
+  Future<void> delete(String key) async {
+    var shared = await SharedPreferences.getInstance();
+    shared.remove(key);
+  }
+
+  @override
+  Future<dynamic> get(String key) async {
+    var shared = await SharedPreferences.getInstance();
+    return shared.get(key);
+  }
+
+  @override
+  Future<void> put(String key, dynamic value) async {
+    var shared = await SharedPreferences.getInstance();
+    print(
+        "[SharedLocalStorageService] put(String key, dynamic value) - this is the new value to be storaged: $value");
+    if (value is bool) {
+      shared.setBool(key, value);
+    } else if (value is double) {
+      shared.setDouble(key, value);
+    } else if (value is int) {
+      shared.setInt(key, value);
+    } else if (value is String) {
+      shared.setString(key, value);
+    } else if (value is List<String>) {
+      shared.setStringList(key, value);
+    }
+  }
+}
